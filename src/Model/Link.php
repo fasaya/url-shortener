6d0585b193133c063e784ab1418fb51bf5f759a3
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-09-11 21:23:37 +0800

    Update model

--- src/Model/Link.php
+++ src/Model/Link.php
@@ -12,11 +12,13 @@ class Link extends Model
     protected $table = 'short_links';
 
     protected $fillable = [
+        'slug',
         'short_url',
         'long_url',
+        'description',
         'clicks',
-        'secret_key',
         'is_disabled',
+        'expired_at',
         'created_by',
         'creator_ip',
         'deleted_by',
@@ -30,53 +32,68 @@ class Link extends Model
         return $this->hasMany(LinkClick::class);
     }
 
-    public static function add($long_url, Model $user): self
+    public static function add($long_url, User $user = null): self
     {
         self::validUrl($long_url);
 
-        $short_url = self::generateShortUrl($long_url);
+        $generate = self::generateShortUrl($long_url);
 
-        if (!static::hasAllowedValues($value)) {
-            throw InvalidMarkValueException::create();
-        }
+        return static::create([
+            'slug' => $generate['slug'],
+            'long_url' => $long_url,
+            'short_url' => $generate['url'],
+            'description' => null,
+            'expired_at' => config('url-shortener.expire-days') ? date('Y-m-d H:i:s', strtotime('+' . config('url-shortener.expire-days') . ' days', strtotime(now()))) : null,
+            'created_by' => $user ? $user->id : null,
+            'creator_ip' => request()->ip(),
+        ]);
+    }
 
-        $attributes = [
-            app(static::class)->getUserIdColumn() => $user->getKey(),
-            'markable_id' => $markable->getKey(),
-            'markable_type' => $markable->getMorphClass(),
-            'value' => $value,
-        ];
 
-        $values = static::forceSingleValuePerUser()
-            ? [Arr::pull($attributes, 'value')]
-            : [];
+    public static function addCustom($long_url, $customSlug, User $user = null): self
+    {
+        self::validUrl($long_url);
 
-        return static::firstOrCreate($attributes, $values);
+        $short_url = self::generateCustomUrl($customSlug);
+
+        return static::create([
+            'slug' => $customSlug,
+            'long_url' => $long_url,
+            'short_url' => $short_url,
+            'description' => null,
+            'expired_at' => config('url-shortener.expire-days') ? date('Y-m-d H:i:s', strtotime('+' . config('url-shortener.expire-days') . ' days', strtotime(now()))) : null,
+            'created_by' => $user ? $user->id : null,
+            'creator_ip' => request()->ip(),
+        ]);
     }
 
     public static function exists($short_url): bool
     {
-        return static::where('short_url', $short_url)->where('is_disabled', 0)->exists();
+        return static::where('short_url', $short_url)
+            ->where('is_disabled', 0)
+            ->where('expired_at', '>', now())
+            ->exists();
     }
 
     public static function validUrl($url): bool
     {
         if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-            // throw exception
+            throw new \Exception('Not a valid URL.');
         }
 
         return true;
     }
 
-    public static function generateShortUrl($long_url, $custom_string = NULL): string
+    public static function generateShortUrl($long_url): array
     {
-        $short_url = Str::random(10);
+        $slug = Str::random(10);
+        $short_url = config('app.url') . config('url-shortener.url-prefix') . '/' . $slug;
 
         if (self::exists($short_url)) {
 
             // check if tries limit is reached
             if (self::$generate_tries_limit <= 0) {
-                throw new \Exception('Tries limit reached');
+                throw new \Exception('Tries limit reached.');
             }
 
             self::$generate_tries_limit--;
@@ -84,13 +101,18 @@ class Link extends Model
             return static::generateShortUrl($long_url);
         }
 
-        return $short_url;
+        return [
+            'slug' => $slug,
+            'url' => $short_url
+        ];
     }
 
-    public static function generateCustomUrl($custom_string): string
+    public static function generateCustomUrl($slug): string
     {
+        $short_url = config('app.url') . config('url-shortener.url-prefix') . '/' . $slug;
+
         if (self::exists($short_url)) {
-            throw new \Exception('Tries limit reached');
+            throw new \Exception('URL already exists.');
         }
 
         return $short_url;
